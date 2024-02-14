8872c1bd1ad55d41ca4fed55f7088aa029a15058
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-02-14 13:52:53 +0100

    He cambiado de BD a mongo Atlas, para una mayor comodidad a la hora de mover datos

--- ProyectoFinal/backend/src/loaders/mongodb-loader.js
+++ ProyectoFinal/backend/src/loaders/mongodb-loader.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import logger from '../utils/logger.js';
 
 export default async function(config){
-  const url = `mongodb://${config.host}:${config.port}/${config.dbName}`;
+  const url = `mongodb+srv://jaseggom:Ja1977-1980@bd-azure.tvlwczp.mongodb.net/?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(url);
     logger.info(`Connected to MongoDB at ${url}`)
