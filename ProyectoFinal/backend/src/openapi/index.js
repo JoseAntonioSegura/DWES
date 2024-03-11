fd7065ecf15f466ef892c22fc2f33f7f829856fc
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-03-11 13:37:58 +0100

    Arreglando el swagger

--- ProyectoFinal/backend/src/openapi/index.js
+++ ProyectoFinal/backend/src/openapi/index.js
@@ -1,27 +1,9 @@
 import yaml from 'js-yaml';
-import { readFileSync } from 'fs';
-import { dirname, resolve } from 'path';
+import {readFileSync} from 'fs';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname)
-// export const swaggerDoc = yaml.load(readFileSync(__dirname+'/openapi.yml', 'utf8'));
+const _dirname = dirname(fileURLToPath(import.meta.url));
+console.log(_dirname);
 
-function parseYaml(file) {
-    return yaml.load(readFileSync(resolve(__dirname, `${file}.yml`), 'utf8'));
-}
-
-export const swaggerDoc = {
-    openapi: "3.0.0",
-    info: {
-        title: "My simple API",
-        description: "A simple API to learn how to write OpenAPI Specification",
-    },
-    paths: parseYaml('paths'),
-    components: {
-        schemas: parseYaml('schemas'),
-        securitySchemes: parseYaml('security'),
-        examples: parseYaml('examples'),
-        responses: parseYaml('responses'),
-    },
-};
+export const swaggerDoc = yaml.load(readFileSync(_dirname+'/openapi.yml', 'utf-8'));
