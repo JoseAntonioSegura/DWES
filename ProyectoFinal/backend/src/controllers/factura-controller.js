03c92dcaf220a8f0bd23f7bc5bca429591e552a0
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-03-21 14:40:00 +0100

    Update factura-controller.js

--- ProyectoFinal/backend/src/controllers/factura-controller.js
+++ ProyectoFinal/backend/src/controllers/factura-controller.js
@@ -1,6 +1,7 @@
 import * as FacturaService from '../services/database/factura-db-service.js';
 import { HttpStatusError } from 'common-errors';
 
+
 // Agregar una factura
 export const agregarFactura = async (req, res) => {
   const { userId, productos } = req.body;
