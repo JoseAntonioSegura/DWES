c60988003cfdc314eba6e061330a82568d975092
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-05-26 03:11:21 +0200

    cambios por encima

--- ProyectoFinal/frontend/src/components/facturas/facturas.js
+++ ProyectoFinal/frontend/src/components/facturas/facturas.js
@@ -103,7 +103,7 @@ const FacturasModal = ({ onClose }) => {
                                     {productosDetalles.map((producto, index) => (
                                         <div key={index} className="producto-detalle">
                                             <img src={producto.detalles.imagen} alt={producto.detalles.titulo} className="producto-imagen" />
-                                            <p><strong>Producto:</strong> {producto.detalles.titulo}</p>
+                                            <p>{producto.detalles.titulo}</p>
                                             <p><strong>Cantidad:</strong> {producto.unidades}</p>
                                             <p><strong>Precio:</strong> {producto.precio}€</p>
                                             <p><strong>Total:</strong> {Math.round(producto.precio * producto.unidades * 100) / 100}€</p>
